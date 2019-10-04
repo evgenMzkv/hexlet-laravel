bdd7fb4f69684e3d4bb252f93d7105374788b255
Evgeniy Myznikov <evgen.myznikov@gmail.com>
2019-10-04 20:57:28 +0300

    Layouts ex.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('header')
+    <h1>О блоге</h1>
+@endsection
+
+@section('content')
+    <p>Эксперименты с Ларавелем на Хекслете</p>
+@endsection
